d816f233cc55810dd2ee8a18f6dcc0fcd3cb0565
Saar <saargrin@gmail.com>
2018-01-07 13:22:04 +0200

    more chars

--- matrix.sh
+++ matrix.sh
@@ -5,7 +5,26 @@ cols=$(tput cols)
 
 awkscript='
   {
-    letters="･ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ∑ΣΨΩΛΔβδζξφ𝞷𝟂㋐㋑㋒㋓㋔㋕㋖㋗㋘㋙㋚㋛㋜㋝㋞㋟㋠㋡㋢㋣㋤㋥㋦㋧㋨㋩㋪㋫㋬㋭㋮㋯㋰㋱㋲㋳㋴㋵㋶㋷㋸㋹㋺㋻㋼㋽㋾"
+    letters="･ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝमॶळओΔβλφψωρμΞΨδאש"
+    lines=$1
+    random_col=$3
+    c=$4
+    letter=substr(letters,c,1)
+    cols[random_col]=0;
+    for (col in cols) {
+      line=cols[col];
+      cols[col]=cols[col]+1;
+      printf "\033[%s;%sH\033[2;32m%s", line, col, letter;
+      printf "\033[%s;%sH\033[1;37m%s\033[0;0H", cols[col], col, letter;
+      if (cols[col] >= lines) {
+        cols[col]=0;
+      }
+    }
+  }
+'
+awkscript2='
+  {
+    letters="                                                                            "
     lines=$1
     random_col=$3
     c=$4
@@ -23,10 +42,21 @@ awkscript='
   }
 '
 
+while true;do
 echo -e "\e[1;40m"
 clear
-
-while :; do
+i=0
+while [ $i -lt 200 ] ;do
   echo $lines $cols $(( $RANDOM % $cols)) $(( $RANDOM % 72 ))
-  sleep 0.05
+  sleep 0.08
+  i=$(($i + 1 ))
+#  echo "$i" > /tmp/i.tmp
 done | awk "$awkscript"
+
+while [ $i -lt 200 ] ;do
+  echo $lines $cols $(( $RANDOM % $cols)) $(( $RANDOM % 72 ))
+  sleep 0.08
+  i=$(($i + 1 ))
+#  echo "$i" > /tmp/i.tmp
+done | awk "$awkscript2"
+done
